c99e7835d4f6cbbb41714f8fa6a5bc132435ac3d
Manuel Rossi <63568548+manurdls@users.noreply.github.com>
2020-05-21 21:15:47 -0300

    Update manejadorInscripciones.py

--- manejadorInscripciones.py
+++ manejadorInscripciones.py
@@ -1,3 +1,5 @@
+import csv
+
 import numpy as np
 
 from datetime import date
@@ -34,9 +36,40 @@ class manejadorInscripciones(object):
         return s
 
     def consultarInscripciones(self, idTaller):
+        band = False
         s = 'DNI             Nombre                   Direccion\n'
         for i in range(len(self.__inscripciones)):
             if type(self.__inscripciones[i]) == Inscripcion:
                 if self.__inscripciones[i].compararIdTaller(idTaller) == True:
                     s += self.__inscripciones[i].getDatosPersona() + '\n'
+                    band = True
+        if band == False:
+            s = 'No hay personas inscriptas en este taller.'
         return s
+
+    def registrarPago(self, dni):
+        band = False
+        i = 0
+        while ((i < len(self.__inscripciones)) & (not band)):
+            if self.__inscripciones[i].compararDni(dni) == True:
+                band = True
+                if self.__inscripciones[i].registrarPago() == True:
+                    print('Pago registrado.')
+                else:
+                    print('Error: la persona está al día.')
+            else:
+                i += 1
+
+    def guardarInscripciones(self):
+        archivo = open('inscripciones.csv', 'w', newline='')
+        salida = csv.writer(archivo)
+        band = False
+        for i in self.__inscripciones:
+            if type(i) == Inscripcion:
+                salida.writerow([i.getDniPersona(), i.getIdTaller(), i.getFecha(), i.getPago()])
+                band = True
+        archivo.close()
+        if band == True:
+            print('Inscripciones guardadas.')
+        else:
+            print('No hay inscripciones registradas.')
